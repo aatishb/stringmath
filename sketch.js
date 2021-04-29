fad8917796c1f9579ca39c087f1d84105dd94a26
Aatish Bhatia <aatish@gmail.com>
2021-04-29 14:27:03 -0400

    stripped down template to essentials

---
+++ sketch.js
@@ -0,0 +1,47 @@
+// this p5 sketch is written in instance mode
+// read more here: https://github.com/processing/p5.js/wiki/Global-and-instance-mode
+
+function sketch(parent) { // we pass the sketch data from the parent
+  return function( p ) { // p could be any variable name
+
+    if (parent.data.code) {
+      try {
+        eval(parent.data.code);
+      } catch(error) {
+        console.log(parent.data.code);
+        console.log(error);
+      }
+    }
+
+    p.setup = function() {
+      let canvas = p.createCanvas(400, 400);
+      canvas.parent(parent.$el);
+
+      setup(p, parent.data.vars);
+    };
+
+
+    p.draw = function() {
+      if (parent.isVisible) {
+        draw(p, parent.data.vars);
+      }
+    };
+
+    /*
+
+    p.mouseClicked = function() {
+      if (p.mouseY > 0 && p.mouseY < p.height) {
+        //console.log(parent.data);
+      }
+    };
+
+    // this is a new function we've added to p5
+    // it runs only if the data changes
+    p.dataChanged = function(val, oldVal) {
+      // console.log('data changed');
+      // console.log('x: ', val.x, 'y: ', val.y);
+    };
+    */
+
+  };
+}
--- vue-definitions.js
+++ vue-definitions.js
@@ -1,82 +1,3 @@
-Vue.component('sound', {
-
-  template: `
-    <div>
-      <button class="button" @click="playString(1, 0.1)" style="margin-top: 5px;">PLAY</button>
-      <button class="button" @click="strum([1, 5/4, 3/2])" style="margin-top: 5px;">STRUM MAJOR CHORD</button>
-      <button class="button" @click="strum([1, 6/5, 3/2])" style="margin-top: 5px;">STRUM MINOR CHORD</button>
-    </div>
-  `,
-
-  props: ['sin', 'cos', 'freq', 'N', 'bow'],
-
-  methods: {
-    strum(arr) {
-      this.playString(arr[0], 0.1);
-      this.playString(arr[1], 0.6);
-      this.playString(arr[2], 1.1);
-
-      this.playString(arr[0], 2);
-      this.playString(arr[1], 2.1);
-      this.playString(arr[2], 2.2);
-    },
-
-    playString(freqMultiplier, deltaT) {
-      let N = this.N;
-
-      let zero = new Float32Array(N+1).fill(0);
-
-      let real;
-      let img;
-
-      let sum = 0;
-
-      let oscArray = [];
-
-      for (let n = 1; n <= N; n++) {
-        real = zero.slice();
-        img = zero.slice();
-
-        real[n] = this.cos ? this.cos(n) : 0;
-        img[n] = this.sin ? this.sin(n) : 0;
-
-        let osc = context.createOscillator();
-        let wave = context.createPeriodicWave(real, img, {disableNormalization: true});
-
-        osc.setPeriodicWave(wave);
-        osc.frequency.value = this.freq * freqMultiplier;
-
-        if ( Math.abs(real[n]) > epsilon || Math.abs(img[n]) > epsilon ) {
-
-          sum += Math.abs(real[n]);
-          sum += Math.abs(img[n]);
-
-          oscArray.push({
-            osc: osc,
-            index: n
-          });
-        }
-      }
-
-      let T = context.currentTime;
-      let fadeTime = 1;
-
-      let maxGain = 0.5/sum;
-
-      for (let osc of oscArray) {
-        let n = osc.index;
-        let o = osc.osc;
-        if (this.bow) {
-          play(o, T + deltaT, 1, 1);
-        } else {
-          fade(o, T + deltaT, fadeTime/n, maxGain);
-        }
-      }
-
-    }
-  }
-
-}),
 
 Vue.component('animation', {
 
@@ -84,10 +5,10 @@ Vue.component('animation', {
   <div>
     <div class="center">
 
-      <div class="center-inline" v-for="s in sliders">
+      <div v-if="sliders" class="center-inline" v-for="s in sliders">
         <div> {{s[0]}} {{getVarName(s)}} = {{vars[getVarName(s)]}} </div>
         &nbsp;&nbsp;
-        <input type="range" :min="s[1]" :max="s[2]" :step="s[3]"
+        <input type="range" :min="s[1]" :max="s[2]" :step="s[3]" 
           v-model.number="vars[getVarName(s)]"></input>
       </div>
 
@@ -95,12 +16,12 @@ Vue.component('animation', {
 
     </div>
 
-  <p5 v-if="code" src="./drawGraph.js" :data="{vars: vars, graph: graph, marker: marker}"></p5>
+  <p5 v-if="code" src="./sketch.js" :data="{vars: vars, code: code}"></p5>
 
   </div>
   `,
 
-  props: ['vars', 'sliders', 'code', 'marker'],
+  props: ['vars', 'sliders', 'code'],
 
   methods: {
     getVarName(val) {
@@ -114,33 +35,11 @@ Vue.component('animation', {
 
   data: function() {
     return {
-      graph: NaN
     }
   },
 
   mounted() {
-    //let code = this.code;
-    //this.$refs.animation.innerHTML = '';
-    //console.log('animation component mounted');
 
-    if (this.code) {
-      try {
-
-        eval(this.code);
-        // console.log(code);
-        this.graph = graph;
-        // console.log(this.vars);
-        //console.log(this.sliders['a']);
-        //console.log(this.getSliderName(this.sliders['a']));
-        // console.log(this.graph(0.5, 0.1, ...Object.values(this.vars)));
-
-      } catch(error) {
-
-        console.log(code);
-        console.log(error);
-
-      }
-    }
   }
 
 });
@@ -289,9 +188,6 @@ Vue.component('p5', {
 
     visibilityChanged(isVisible, entry) {
       this.isVisible = isVisible
-      if(this.myp5.playPause) {
-        this.myp5.playPause(isVisible);
-      }
     }
   },
 
@@ -325,73 +221,13 @@ let app = new Vue({
 
   methods: {
 
-    struckString(n) {
-      let l = this.struckstring.l;
-      return Math.sin(n * Math.PI * l)/(Math.PI * n);
-    },
-
-    struckStringFW(n) {
-      let l = this.struckstring.l;
-      let d = this.struckstring.d;
-      return 2 * Math.sin(n * Math.PI * l) * Math.sin(n * Math.PI * d/2) / Math.pow((Math.PI * n),2);
-    },
-
-    pluckedString(n) {
-      let l = this.pluckedstring.l;
-      return (2/(l * (1-l))) * Math.sin(n * Math.PI * l) / Math.pow((n * Math.PI),2);
-    },
-
-    bowedString(n) {
-      let l = this.bowedstring.l;
-      return (1/(l * (1-l))) * Math.sin(n * Math.PI * l) / Math.pow((n * Math.PI),3);
-    },
-
   },
 
   computed: {
 
   },
 
-
   data: {
-
-    freq: 220,
-    playing: false,
-
-    stringnormalmodes: {
-      n: 1,
-      a: 1,
-      b: 0
-    },
-
-    pluckedstring: {
-      N: 10,
-      l: 0.1,
-      h: 1
-    },
-
-    bowedstring: {
-      N: 10,
-      l: 0.1,
-      v: 1
-    },
-
-    struckstring: {
-      N: 10,
-      l: 0.1,
-      d: 0.1
-    },
-
-    mixingmodes: {
-      c: 0,
-      A1: 0,
-      A2: 0,
-      A3: 0,
-      A4: 0,
-      A5: 0,
-      A6: 0,
-    }
-
   }
 
 })
